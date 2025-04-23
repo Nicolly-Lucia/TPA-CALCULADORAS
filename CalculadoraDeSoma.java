f44d3b28dbe9fa3c19dced0b59c78b601516615f
Nicolly Lucia Da Silva <nicklucia0401@gmail.com>
2025-04-23 14:05:51 -0300

    Add files via upload

---
+++ CalculadoraDeSoma.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoradesoma;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class CalculadoraDeSoma {
+
+    public static void main(String[] args) {
+      // Aqui nós vamos realizar uma cálculadora simples de soma entre dois números.
+     
+      double numero1 = 274.2;
+      double numero2 = 192.5;
+      //double armazena números com casas decimais.
+      
+      //agora vamos realizar a soma:
+      
+      double Soma = numero1 + numero2;
+      
+      //vamos exibir os resultados:
+      
+      System.out.println("Resultado da Soma");
+      System.out.println("Soma: " + Soma);
+      //System.out chama os dados estáticos.
+    }
+}
